78ff93111d83d1d3aeeaf0bbb349be4c140107f3
jonatan-c <jona.cc.93@gmail.com>
2024-01-06 22:54:18 -0300

    another fix analytics 4

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,10 +7,10 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
        <Script
-         
+          strategy="afterInteractive"
           src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
         />
-        <Script id="google-analytics">
+        <Script strategy="afterInteractive" id="google-analytics">
           {`
 							window.dataLayer = window.dataLayer || [];
 							function gtag(){dataLayer.push(arguments);}
