448186882d123cfae06c0833f042acb01cf5fb0e
jonatan-c <jona.cc.93@gmail.com>
2024-01-06 22:28:06 -0300

    add google analytics

--- components/layouts/MainLayout.tsx
+++ components/layouts/MainLayout.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import Script from 'next/script';
 import React, { useEffect } from 'react';
 
 interface Props {
@@ -14,7 +15,26 @@ export const MainLayout = ({ children }: Props): any => {
 				<title>Jonatan Claros</title>
 				<meta name="description" content="portfolio" />
 				<meta name="viewport" content="width=device-width, initial-scale=1" />
-				{/* <link rel="icon" href="/favicon.ico" /> */}
+				{/* <script async src="https://www.googletagmanager.com/gtag/js?id=G-DBKMDQSZWE"></script>
+				<script>
+				window.dataLayer = window.dataLayer || [];
+				function gtag(){dataLayer.push(arguments);}
+				gtag('js', new Date());
+
+				gtag('config', 'G-DBKMDQSZWE');
+				</script> */}
+				<Script src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
+					<Script id="google-analytics">
+						{`
+						window.dataLayer = window.dataLayer || [];
+						function gtag(){dataLayer.push(arguments);}
+						gtag('js', new Date());
+				
+						gtag('config',  '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+							page_path: window.location.pathname,
+						});
+					`}
+				</Script>
 			</Head>
 			<main>{children}</main>
 		</>
